472d644e6f818f62b3936ebdba7bbfd8329eb0a2
Arkadiusz <areq12x@gmail.com>
2024-01-02 20:42:53 +0100

    ignore typescript

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const redirects = require('./redirects')
 
 const nextConfig = {
   typescript: {
-    ignoreBuilderros: true,
+    ignoreBuildErrors: true,
   },
   reactStrictMode: true,
   swcMinify: true,
